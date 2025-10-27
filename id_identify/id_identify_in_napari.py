eff77f8e3b40fb097ccf1706ee61e25ede5dbb52
JH Wang <1125623896@qq.com>
2025-10-27 19:05:11 +0800

    fix: modify draw_neuron_box interface

--- id_identify/id_identify_in_napari.py
+++ id_identify/id_identify_in_napari.py
@@ -1,8 +1,3 @@
-if __name__ == "__main__":
-    green_file_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250522_LThe\w5_2025-05-22_16-37-58\green.npy"
-    red_file_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250522_LThe\w5_2025-05-22_16-37-58\red.npy"
-    aligned_volume_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250522_LThe\w5_2025-05-22_16-37-58\aligned_volumes_mip.npy"
-    neuron_pt_tuple_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250522_LThe\w5_2025-05-22_16-37-58\all_neuron_pt_tuple.npy"
 # %%
 import numpy as np
 import napari
@@ -22,30 +17,29 @@ def image_process(image_np, background_value = 110):
     image_np[image_np < background_value] = 0  # Set background pixels to 0
     return image_np
 
-def image_and_box_load(green_file_path, red_file_path,  neuron_pt_tuple_path , aligned_volume_path = None):
-    """_summary_
-
+def image_and_box_load(green_file_path, red_file_path,  neuron_pt_tuple_path , root_volume_path = None, root_neuron_pt_tuple = None):
+    """
     Args:
-        green_file_path (_type_): green channel images (2vols)
-        red_file_path (_type_): red channel image (2vols)
-        neuron_pt_tuple_path (_type_, optional): neuron_pt_tuple(x,y,z*5,w,h,d)
-        aligned_volume_path (_type_, optional): aligned MIP volume 
-
+        green_file_path (_type_): green channel volume npy
+        red_file_path (_type_): red channel volume npy
+        neuron_pt_tuple_path (_type_, optional): neuron pt tuple npy
+        root_volume_path (_type_, optional): aligned MIP volume 
+        root_neuron_pt_tuple (_type_, optional): neuron pt tuple for aligned MIP volume
     Returns:
         processed green array and red array 
     """
     green = np.load(green_file_path)
     red = np.load(red_file_path)
-    if aligned_volume_path is not None:
-        aligned_volume_npy = np.load(aligned_volume_path)
-        green[1] = aligned_volume_npy.transpose(2,0,1)
-
+    neuron_pt_tuple = np.load(neuron_pt_tuple_path)
+    if root_volume_path is not None:
+        root_volume = np.load(root_volume_path)
+        green[1] = root_volume.transpose(2,0,1)
+        if root_neuron_pt_tuple is not None:
+            neuron_pt_tuple[1] = np.load(root_neuron_pt_tuple)
     green = image_process(green)
     red = image_process(red)
-    neuron_pt_tuple = np.load(neuron_pt_tuple_path)
     output_shape = (2,18,1024,1024)
-    mask = draw_neuron_box(neuron_pt_tuple[:2],output_shape=output_shape, if_save=False)
-
+    mask = draw_neuron_box(neuron_pt_tuple[:2],output_shape=output_shape, save_dir=None)
     return green, red, mask
 
 def view_in_napari(green, red, mask, mask_name = "mask",green_scale=[1,5,1,-1], red_scale = [1,5,1,1], red_translate = [0,0,0,0], green_translate = [0,0,0,1024], mask_scale=[1,5,1,-1], mask_translate=[0,0,0,1024]):
@@ -60,7 +54,7 @@ def id_identify_in_napari(green_file_path, red_file_path,  neuron_pt_tuple_path 
     green, red, mask = image_and_box_load(green_file_path=green_file_path,
                                           red_file_path=red_file_path,
                                           neuron_pt_tuple_path=neuron_pt_tuple_path,
-                                          aligned_volume_path=aligned_volume_path)
+                                          root_volume_path=aligned_volume_path)
     
     viewer = view_in_napari(green=green,
                             red=red,
@@ -78,6 +72,11 @@ def id_identify_in_napari(green_file_path, red_file_path,  neuron_pt_tuple_path 
 
 # %%
 if __name__ == "__main__":
+    green_file_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250712\w1\green.npy"
+    red_file_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250712\w1\red.npy"
+    aligned_volume_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250712\w1\synthetic_volume\aligned_volumes_mip.npy"
+    neuron_pt_tuple_path = r"H:\Process_temporary\WJH\olfactory\ID\image_data\20250712\w1\synthetic_volume\all_neuron_pt_tuple.npy"
+    
     viewer = id_identify_in_napari(
         green_file_path=green_file_path,
         red_file_path=red_file_path,
