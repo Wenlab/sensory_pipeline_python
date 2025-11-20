857d610965a658b5af098d5d23b4e37f688e775c
JH Wang <1125623896@qq.com>
2025-11-20 17:12:09 +0800

    feat: enhance interpolation function to support DataFrame input and improve error handling

--- utils/interpolate.py
+++ utils/interpolate.py
@@ -1,31 +1,70 @@
 import numpy as np
+import pandas as pd
+
+
 def interpolate_over_nans(input_mat, t=None):
-    """ Function to interpolate over NaN values along the second dimension of a matrix
+    """Function to interpolate over NaN values along the second dimension of a matrix.
 
     Args:
-        input_mat: numpy array, 1D or [neurons, time] 2D matrix with NaN values
-        t: optional time vector, only useful if input_mat is not sampled regularly in time
+        input_mat: numpy array, 1D or [neurons, time] 2D matrix with NaN values. A
+            pandas DataFrame of shape [neurons, time] is also supported.
+        t: optional time vector, only useful if input_mat is not sampled regularly in time.
 
-    Returns: Interpolated input_mat, interpolated time
+    Returns:
+        Tuple containing the interpolated matrix (matching the input type) and the
+        generated/interpolated time vector.
     """
-    def check_input_format(data):
-        if type(data) is not np.ndarray:
+
+    def check_numpy_input(data):
+        if not isinstance(data, np.ndarray):
             raise Exception('The red and green matricies must be the numpy arrays')
 
-        if data.ndim != 1 and data.ndim != 2:
+        if data.ndim not in (1, 2):
             raise Exception('The red and green matricies should be 1 or 2 dimensional')
 
         if data.ndim == 1:
-            data = data[None, :] # Make it [1, time] instead of [time, 1]
+            data = data[None, :]  # Make it [1, time] instead of [time, 1]
 
         return data
 
-    # Check and prepare input
-    input_mat = check_input_format(input_mat)
-    
+    def interpolate_dataframe(df_input, t_vals):
+        df_numeric = df_input.copy()
+
+        if not all(np.issubdtype(dtype, np.number) for dtype in df_numeric.dtypes):
+            raise TypeError('All DataFrame columns must be numeric for interpolation')
+
+        df_numeric = df_numeric.astype(float)
+
+        if t_vals is None:
+            t_vals = np.arange(df_numeric.shape[1])
+        else:
+            t_vals = np.asarray(t_vals)
+            if t_vals.ndim != 1:
+                raise ValueError('t must be a 1D array-like when provided')
+            if t_vals.size != df_numeric.shape[1]:
+                raise ValueError('The length of t must match the number of time points')
+
+        sample_rate = 1 / np.mean(np.diff(t_vals, axis=0))
+        t_interp_vals = np.arange(df_numeric.shape[1]) / sample_rate
+
+        interp_df = df_numeric.interpolate(axis=1, method='linear', limit_direction='both')
+
+        all_nan_cols = df_numeric.columns[df_numeric.isna().all()]
+        for col in all_nan_cols:
+            print('column ' + str(col) + ' is all NaN, skipping')
+            interp_df[col] = 0.0
+
+        return interp_df, t_interp_vals
+
+    if isinstance(input_mat, pd.DataFrame):
+        return interpolate_dataframe(input_mat, t)
+
+    # Check and prepare numpy input
+    input_mat = check_numpy_input(input_mat)
+
     # Transpose to [time, neurons] for processing
     input_mat = input_mat.T
-    
+
     # if t is not specified, assume it has been sampled at regular intervals
     if t is None:
         t = np.arange(input_mat.shape[0])
