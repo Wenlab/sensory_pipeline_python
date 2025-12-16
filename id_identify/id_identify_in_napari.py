94de74044e3397453d693b85b17e5d266408a52b
JH Wang <1125623896@qq.com>
2025-12-16 13:48:01 +0800

    fix: add contrast limit parameter in the main function

--- id_identify/id_identify_in_napari.py
+++ id_identify/id_identify_in_napari.py
@@ -84,6 +84,8 @@ def id_identify_in_napari(green_file_path, red_file_path,  neuron_pt_tuple_path 
         green_translate (_type_, optional): green channel translate in napari viewer
         mask_scale (_type_, optional): mask scale in napari viewer
         mask_translate (_type_, optional): mask translate in napari viewer
+        red_contrast_limits (_type_, optional): red channel contrast limits in napari viewer
+        green_contrast_limits (_type_, optional): green channel contrast limits in napari viewer
     """
     mask_name = kwargs.get("mask_name", "mask")
     green_scale = kwargs.get("green_scale", [1,5,1,-1])
@@ -92,7 +94,8 @@ def id_identify_in_napari(green_file_path, red_file_path,  neuron_pt_tuple_path 
     green_translate = kwargs.get("green_translate", [0,0,0,1024])
     mask_scale = kwargs.get("mask_scale", [1,5,1,-1])
     mask_translate = kwargs.get("mask_translate", [0,0,0,1024])
-    
+    red_contrast_limits = kwargs.get("red_contrast_limits", [120,250])
+    green_contrast_limits = kwargs.get("green_contrast_limits", [120,250])
     green, red, mask = image_and_box_load(green_file_path=green_file_path,
                                           red_file_path=red_file_path,
                                           neuron_pt_tuple_path=neuron_pt_tuple_path,
@@ -106,7 +109,9 @@ def id_identify_in_napari(green_file_path, red_file_path,  neuron_pt_tuple_path 
                             green_translate=green_translate,
                             red_translate=red_translate,
                             mask_scale=mask_scale,
-                            mask_translate=mask_translate)
+                            mask_translate=mask_translate,
+                            red_contrast_limits=red_contrast_limits,
+                            green_contrast_limits=green_contrast_limits)
 
     
     return viewer
