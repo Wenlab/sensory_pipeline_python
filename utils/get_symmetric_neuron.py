402a8e309b6478d6f8dfdcda8ff739552f51f700
JH Wang <1125623896@qq.com>
2025-12-29 14:31:59 +0800

    chore: delete useless function

--- utils/get_symmetric_neuron.py
+++
@@ -1,8 +0,0 @@
-def get_symmetric_neuron(neuron_name):
-    if isinstance(neuron_name, str):
-        if neuron_name.endswith('L'):
-            return neuron_name[:-1] + 'R'
-        elif neuron_name.endswith('R'):
-            return neuron_name[:-1] + 'L'
-        else:
-            return None
