d932709745e6150ac70d1e32e79c17eda76ba883
JH Wang <1125623896@qq.com>
2026-02-03 13:04:53 +0800

    refactor: enhance prepare_signal_dict to support raw data processing and optional labjack path
    fix: fix kwargs error in draw_waterfall_plot

--- result_plot/draw_single_signal_plotly.py
+++ result_plot/draw_single_signal_plotly.py
@@ -9,11 +9,20 @@ from result_analysis.single_trial_analyse import cluster_by_corr_then_sort_by_de
 from utils.interpolate import interpolate_over_nans
 from utils.vis_plotly_utils import add_regions_to_fig, draw_waterfall_plot
 
-def prepare_signal_dict(h5_file_path, root_name, labjack_excel_path):
+def prepare_signal_dict(h5_file_path, root_name, labjack_excel_path=None, raw_data=False):
     intensity, _, n_seq, _ = _load_single_trial_intensity(h5_file_path, root_name)
-    stimulus_intervals, stimulus_list = _resolve_stimulus_metadata(labjack_excel_path, root_name)
-    delta_f, fitted, quality = _prepare_delta_f(intensity, stimulus_intervals, n_seq, baseline_pre=6, baseline_post=1, vps_setting=1, background_noise=102)
-    delta_f_interp, t_inter = interpolate_over_nans(delta_f)
+    if labjack_excel_path is not None:
+        stimulus_intervals, stimulus_list = _resolve_stimulus_metadata(labjack_excel_path, root_name)
+    else:
+        stimulus_intervals, stimulus_list = [], []
+
+    if raw_data:
+        delta_f = intensity - np.nanmean(intensity, axis=1, keepdims=True)
+        delta_f_interp, t_inter = interpolate_over_nans(delta_f)
+    else:
+        delta_f, fitted, quality = _prepare_delta_f(intensity, stimulus_intervals, n_seq, baseline_pre=6, baseline_post=1, vps_setting=1, background_noise=102)
+        delta_f_interp, t_inter = interpolate_over_nans(delta_f)
+
     signal_dict = {id:sig for id, sig in enumerate(delta_f_interp)}
     return signal_dict, stimulus_intervals, stimulus_list
 
@@ -34,8 +43,8 @@ def map_id(id_excel_path, root_name):
             id_name_dict[digital_id] = biological_name
     return id_name_dict
 
-def prepare_single_signal_plotly(h5_file_path, root_name, labjack_excel_path, id_excel_path=None, color_scheme=None, stim_name=None):
-    signal_dict, stimulus_intervals, stimulus_list = prepare_signal_dict(h5_file_path, root_name, labjack_excel_path)
+def prepare_single_signal_plotly(h5_file_path, root_name, labjack_excel_path=None, id_excel_path=None, color_scheme=None, stim_name=None, raw_data=False):
+    signal_dict, stimulus_intervals, stimulus_list = prepare_signal_dict(h5_file_path, root_name, labjack_excel_path, raw_data=raw_data)
     if id_excel_path is None:
         id_name_dict = {id:id for id in signal_dict.keys()}
     else:
@@ -48,13 +57,15 @@ def prepare_single_signal_plotly(h5_file_path, root_name, labjack_excel_path, id
 
     return signal_dict, stimulus_intervals, stimulus_name_list, color_list, id_name_dict
 
-def create_plotly_fig(h5_file_path, root_name, labjack_excel_path, id_excel_path=None, cluster=False, color_scheme=None, stim_name=None, **kwargs):
-    signal_dict, stimulus_intervals, stimulus_name_list, color_list, id_name_dict = prepare_single_signal_plotly(h5_file_path, root_name, labjack_excel_path, id_excel_path, color_scheme, stim_name)
+def create_plotly_fig(h5_file_path, root_name, labjack_excel_path=None, id_excel_path=None, cluster=False, color_scheme=None, stim_name=None, raw_data=False, **kwargs):
+    signal_dict, stimulus_intervals, stimulus_name_list, color_list, id_name_dict = prepare_single_signal_plotly(h5_file_path, root_name, labjack_excel_path, id_excel_path, color_scheme, stim_name, raw_data=raw_data)
     if cluster:
         sorted_ids, sorted_corr_matrix, sorted_delay_matrix, fig_corr, fig_delay = cluster_by_corr_then_sort_by_delay(signal_dict, max_lag=5,ref_method='center')
     else:
         sorted_ids = sorted(signal_dict.keys())
         fig_corr, fig_delay = None, None
+    region_alpha = kwargs.pop('region_alpha', 0.3)
+    show_region_legend = kwargs.pop('show_region_legend', True)
     fig_signal = draw_waterfall_plot(
         x= np.arange(signal_dict[0].shape[0]),
         y_dict=signal_dict,
@@ -69,8 +80,8 @@ def create_plotly_fig(h5_file_path, root_name, labjack_excel_path, id_excel_path
         stimulus_intervals,
         stimulus_name_list,
         color=color_list,
-        alpha=kwargs.pop('region_alpha', 0.3),
-        showlegend=kwargs.pop('show_region_legend', True),
+        alpha=region_alpha,
+        showlegend=show_region_legend,
     )
 
     return fig_signal, fig_corr, fig_delay
